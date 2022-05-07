083bf2b275b95440a99dd54f845c0a8d59874294
Uchchash346 <mru416@gmail.com>
2022-05-07 15:02:07 +0600

    Intialize and lifecycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'freecodecamp';
+export class AppComponent implements OnInit, OnDestroy {
+  
+  intervalSub:ReturnType<typeof setInterval> | undefined;
+
+  ngOnInit() {
+    this.intervalSub = setInterval(() => {
+      console.log("Hello from ngOnInit")
+    }, 9000);
+  }
+
+  ngOnDestroy() {
+    if (this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
 }
